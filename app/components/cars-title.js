6d755f21ad7122a7eba5f8ee54fd068e4b066ec0
Rudy Jauregui <rudy.jauregui@orbitz.com>
2016-01-12 13:03:24 -0600

    Edits to router/title

--- app/components/cars-title.js
+++ app/components/cars-title.js
@@ -2,10 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-	titleStrNonMobile: function titleStrNonMobile(){
+	titleRegion: true,
+	/*titleStrNonMobile: function titleStrNonMobile(){
 		var testString = 'This is a test for text.';
 		return testString;
-	},
-	titleRegion: true
-
+	}*/
 });
