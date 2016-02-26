96806f95aebd56546bfbb421a17b3ff492102406
Rudy Jauregui <rjauregui@expedia.com>
2016-02-26 11:32:41 -0600

    Edited json for dates. Shortened carDetails to car.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model(){
-		return Ember.$.getJSON('http://terminal2.expedia.com/x/cars/search?pickupdate=2016-02-13T10%3A00&dropoffdate=2016-02-15T16%3A30&pickuplocation=SFO&dropofflocation=SFO&sort=price&limit=10&apikey=P6xP6GRASTqwsQdYKXmpBBnwuSGUTwJH')
+		return Ember.$.getJSON('http://terminal2.expedia.com/x/cars/search?pickupdate=2016-05-13T10%3A00&dropoffdate=2016-05-15T16%3A30&pickuplocation=SFO&dropofflocation=SFO&sort=price&limit=10&apikey=P6xP6GRASTqwsQdYKXmpBBnwuSGUTwJH')
 		.then(function(data){
 		return data.CarInfoList.CarInfo;
 		});
